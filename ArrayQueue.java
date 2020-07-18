581a56638acb0ba0cdb20aabb75edeff5a3a10c0
姚季 <1543544497@qq.com>
2020-07-18 15:13:42 +0800

    简单数组队列

---
+++ ArrayQueue.java
@@ -0,0 +1,86 @@
+import java.util.Scanner;
+
+public class ArrayQueue {
+    private int maxSize;
+    private int front;//队列头
+    private int rear;//队列尾
+    private int[] arr;
+
+    public ArrayQueue(int maxSize){
+        this.maxSize = maxSize;
+        arr = new int[maxSize];
+        front = -1;//指向队列头的前一个位置
+        rear = -1;//指向队列尾
+    }
+    public boolean isFull(){
+        return rear == maxSize-1;
+    }
+
+    public boolean isEmpty(){
+        return front == rear;
+    }
+
+    public void  addQueue(int data){
+        if (isFull()){
+            System.out.println("队列已满");
+            return;
+        }
+        rear++;
+        arr[rear] = data;
+    }
+
+    public int getQueue(){
+        if (isEmpty()){
+            throw new RuntimeException("队列已空");
+        }
+        front++;
+        return arr[front];
+    }
+
+    public void showQueue(){
+        if (isEmpty()){
+            System.out.println("队列为空");
+            return;
+        }
+        for (int i = 0; i < maxSize; i++) {
+            System.out.println("arr["+i+"]: "+arr[i]);
+        }
+    }
+
+    public static void main(String[] args) {
+        ArrayQueue AQ = new ArrayQueue(3);
+       char key = ' ';
+        Scanner scanner = new Scanner(System.in);
+        boolean loop = true;
+        while (loop){
+            System.out.println("s:显示队列");
+            System.out.println("e:退出队列");
+            System.out.println("a:添加数据");
+            System.out.println("g:取出数据");
+            key = scanner.next().charAt(0);
+            switch (key){
+                case 's':
+                    AQ.showQueue();
+                    break;
+                case 'e':
+                    scanner.close();
+                    loop = false;
+                    break;
+                case 'a':
+                    System.out.println("请输入一个数：");
+                    int value = scanner.nextInt();
+                    AQ.addQueue(value);
+                    break;
+                case 'g':
+                    try {
+                        int res = AQ.getQueue();
+                        System.out.println("取出的数据是："+res);
+                    }catch (Exception e){
+                        System.out.println(e.getMessage());
+                    }
+                    break;
+            }
+        }
+    }
+}
+
