352c528e305000c041fb0afe3b8d1d31357a6706
姚季 <1543544497@qq.com>
2020-07-19 12:04:02 +0800

    简单单向链表

--- LinkedList.java
+++ LinkedList.java
@@ -9,6 +9,26 @@ public class LinkedList {
         temp.setNext(heroNode);
     }
 
+    private void addByNo(HeroNode heroNode){//按no大小顺序插入
+        HeroNode temp = head;
+        while (temp.getNext() != null) {
+            temp = temp.getNext();
+            if (temp.getNo() == heroNode.getNo()){
+                System.out.println("已有英雄，添加失败");
+                return;
+            }
+            if (temp.getNext().getNo() > heroNode.getNo()){
+                break;
+            }
+        }
+        if (temp.getNext() != null) {
+            HeroNode next = temp.getNext();
+            temp.setNext(heroNode);
+            temp.getNext().setNext(next);
+        }else
+            temp.setNext(heroNode);
+    }
+
     private void list(){
         if (head.getNext() == null){
             System.out.println("链表为空");
@@ -25,10 +45,17 @@ public class LinkedList {
         HeroNode hero1 = new HeroNode(1, "宋江", "及时雨");
         HeroNode hero2 = new HeroNode(2, "卢俊义", "玉麒麟");
         HeroNode hero3 = new HeroNode(3, "吴用", "智多星");
+        HeroNode hero4 = new HeroNode(5, "吴用", "智多星");
+        HeroNode hero5 = new HeroNode(7, "吴用", "智多星");
+        HeroNode hero6 = new HeroNode(8, "吴用", "智多星");
         LinkedList linkedList = new LinkedList();
         linkedList.add(hero1);
         linkedList.add(hero2);
         linkedList.add(hero3);
+        linkedList.add(hero5);
+        linkedList.add(hero6);
+        linkedList.addByNo(hero4);
+        linkedList.addByNo(hero4);
         linkedList.list();
     }
 }
@@ -52,6 +79,14 @@ class HeroNode{
 
     void setNext(HeroNode next) {
         this.next = next;
+    }
+
+    int getNo() {
+        return no;
+    }
+
+    public void setNo(int no) {
+        this.no = no;
     }
 
     @Override
